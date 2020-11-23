7193942d683627ca5f748df7a1b6d8b3be3067d2
Shimsy Varkey <shimsyanna@gmail.com>
2020-11-23 16:58:52 -0600

    Create the makeresponsive function and create the svgarea variable. Also clear svgarea if not empty

---
+++ D3_data_journalism/assets/js/app.js
@@ -0,0 +1,17 @@
+// @TODO: YOUR CODE HERE!
+
+// Create the svg area
+// create the responsive function for the page
+
+function makeResponsive() {
+
+    var svgArea = d3.select("#scatter").select("svg");
+
+    // clear svg if not empty
+    if (!svgArea.empty()) {
+        svgArea.remove();
+    }
+
+
+
+}
